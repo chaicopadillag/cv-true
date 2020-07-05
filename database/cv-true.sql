cd79b404a3f888339b2e1d5cb15318a87a2b16e6
codecodero <chaicopadillag@gmail.com>
2020-07-04 20:46:51 -0500

    iniciando proyecto cv true

---
+++ database/cv-true.sql
@@ -0,0 +1,336 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 05-07-2020 a las 03:45:27
+-- Versión del servidor: 10.4.11-MariaDB
+-- Versión de PHP: 7.4.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `cv-true`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estudios`
+--
+
+CREATE TABLE `estudios` (
+  `id_estudio` int(11) NOT NULL,
+  `universidad` text NOT NULL,
+  `especialidad` int(11) NOT NULL,
+  `fecha_inicio` date NOT NULL,
+  `fecha_fin` date NOT NULL,
+  `descripcion` text NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `estado` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `experiencias`
+--
+
+CREATE TABLE `experiencias` (
+  `id_experiencia` int(11) NOT NULL,
+  `empresa` text NOT NULL,
+  `cargo` text NOT NULL,
+  `fecha_inicio` date NOT NULL,
+  `fecha_fin` date NOT NULL,
+  `descripcion` text NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `estado` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `failed_jobs`
+--
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `connection` text NOT NULL,
+  `queue` text NOT NULL,
+  `payload` longtext NOT NULL,
+  `exception` longtext NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `galeria`
+--
+
+CREATE TABLE `galeria` (
+  `id_galeria` int(11) NOT NULL,
+  `titulo` text NOT NULL,
+  `imagen` text NOT NULL,
+  `descripcion` text NOT NULL,
+  `fecha` date NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `esatdo` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `habilidades`
+--
+
+CREATE TABLE `habilidades` (
+  `id_habilidad` int(11) NOT NULL,
+  `habilidad` text NOT NULL,
+  `nivel` int(11) NOT NULL,
+  `descripcion` text NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `estado` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_resets_table', 1),
+(3, '2019_08_19_000000_create_failed_jobs_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `redes_sociales`
+--
+
+CREATE TABLE `redes_sociales` (
+  `id_red_social` int(11) NOT NULL,
+  `red_social` text NOT NULL,
+  `url` text NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `estado` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `roles`
+--
+
+CREATE TABLE `roles` (
+  `id_rol` int(11) NOT NULL,
+  `rol` text NOT NULL,
+  `estado` int(11) NOT NULL,
+  `modulos` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `roles`
+--
+
+INSERT INTO `roles` (`id_rol`, `rol`, `estado`, `modulos`) VALUES
+(1, 'admin', 1, 'todos'),
+(2, 'user', 1, 'modulos');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `apellidos` text NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `direccion` text NOT NULL,
+  `telefono` int(11) NOT NULL,
+  `especialidad` text NOT NULL,
+  `ciudad` text NOT NULL,
+  `pais` text NOT NULL,
+  `edad` int(11) NOT NULL,
+  `estado_civil` text NOT NULL,
+  `foto` text NOT NULL,
+  `genero` tinyint(1) NOT NULL,
+  `rol` int(11) NOT NULL,
+  `frase` text NOT NULL,
+  `resumen` text NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `remember_token` varchar(100) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `apellidos`, `password`, `direccion`, `telefono`, `especialidad`, `ciudad`, `pais`, `edad`, `estado_civil`, `foto`, `genero`, `rol`, `frase`, `resumen`, `email_verified_at`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Code', 'code@gmail.com', '', '$2y$10$xdJV6X.nXL6rZvdU7X/dGu5c4nChdm7vpS4SvFse/TnDxTLfn2PQq', '', 0, '', '', '', 0, '', '', 0, 1, '', '', NULL, NULL, '2020-07-05 06:00:10', '2020-07-05 06:00:10');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `estudios`
+--
+ALTER TABLE `estudios`
+  ADD PRIMARY KEY (`id_estudio`);
+
+--
+-- Indices de la tabla `experiencias`
+--
+ALTER TABLE `experiencias`
+  ADD PRIMARY KEY (`id_experiencia`);
+
+--
+-- Indices de la tabla `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `galeria`
+--
+ALTER TABLE `galeria`
+  ADD PRIMARY KEY (`id_galeria`);
+
+--
+-- Indices de la tabla `habilidades`
+--
+ALTER TABLE `habilidades`
+  ADD PRIMARY KEY (`id_habilidad`);
+
+--
+-- Indices de la tabla `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`);
+
+--
+-- Indices de la tabla `redes_sociales`
+--
+ALTER TABLE `redes_sociales`
+  ADD PRIMARY KEY (`id_red_social`);
+
+--
+-- Indices de la tabla `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id_rol`);
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`),
+  ADD UNIQUE KEY `cel` (`telefono`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `estudios`
+--
+ALTER TABLE `estudios`
+  MODIFY `id_estudio` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `experiencias`
+--
+ALTER TABLE `experiencias`
+  MODIFY `id_experiencia` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `galeria`
+--
+ALTER TABLE `galeria`
+  MODIFY `id_galeria` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `habilidades`
+--
+ALTER TABLE `habilidades`
+  MODIFY `id_habilidad` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `redes_sociales`
+--
+ALTER TABLE `redes_sociales`
+  MODIFY `id_red_social` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id_rol` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
